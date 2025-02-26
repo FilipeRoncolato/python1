2258020fe192d936959cd206710a40c2a46e3c91
FilipeRoncolato <162138438+FilipeRoncolato@users.noreply.github.com>
2025-02-26 17:42:49 -0300

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,22 @@
+# comentário        
+# olá mundo
+print("ola mundo")     
+#variaveis
+
+inteiro = 10
+flutuante = 3.14
+texto = "Python é legalllll"
+print(inteiro)
+print(flutuante)
+print(texto)
+#operacoes matematicas 
+a = 10
+b = 3
+
+print("soma", a+b)
+print("subtracao", a-b)
+print("multiplicacao", a*b)
+print("divisao (float)", a/b)
+print("divisao (inteiro')", a//b)
+print("divisao (resto)", a%b)
+print("pontencia", a**b)
